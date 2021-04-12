56248b141c1bac818d8622a18d5af6811f73d336
Stackbit Code Editor <projects@stackbit.com>
2021-04-12 09:58:06 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Template({data}) {
 
 export const pageQuery = graphql`
     query {
-        markdownRemark(fileAbsolutePath: { regex: "/home.md/" }) {
+        markdownRemark(fileAbsolutePath: { regex: "/indx.md/" }) {
             html
             frontmatter {
                 title
